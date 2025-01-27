5c30e29da7d3fadf73e12bfba2afe67d31b6b94f
Laxyny <kevgreg30@gmail.com>
2025-01-27 21:36:48 +0100

    *FIX* Inutile

--- BACK/routes/userRoutes.js
+++ BACK/routes/userRoutes.js
@@ -2,18 +2,17 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
-const isAdmin = require('../middlewares/isAdminMiddleware');
 
 // Routes Users
 router.get('/users', userController.getAllUsers);
 router.get('/user', authMiddleware, userController.getUserFromToken);
-router.get('/user/:id', userController.getUser);
+router.get('/user/:id', authMiddleware, userController.getUser);
 router.post('/register', userController.createUser);
 router.post('/login', userController.loginUser);
 router.post('/logout', userController.logout);
 
-router.put('/updateUser/:id', userController.updateUser);
-router.delete('/deleteUser/:id', userController.deleteUser);
+router.put('/updateUser/:id', authMiddleware, userController.updateUser);
+router.delete('/deleteUser/:id', authMiddleware, userController.deleteUser);
 
 router.put('/users/:id/permissions', authMiddleware, userController.updateUserPermissions);
 
